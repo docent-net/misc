3423df56f7f8ea2c5848a53f5b4a45e747040f5a
Maciej Lasyk <maciek@lasyk.info>
2016-02-23 16:33:41 +0100

    some new features

--- youtube-scraper/scraper.py
+++ youtube-scraper/scraper.py
@@ -1,22 +1,45 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import requests
 import click
 import pafy
 from bs4 import BeautifulSoup
 from urlparse import urljoin
+from os import path
 
 @click.group(invoke_without_command=True)
 #@click.command()
 @click.option('--dir', '-d',
               help='Directory where video files will be saved',
               required=True)
-@click.option('--url',
-              help='Youtube playlist URL',
+@click.option('--urls', '-u',
+              help='URL for Youtube playlist or path to a file which contains playlists or videos URLS ',
               required=True)
-def fetch(dir, url):
-    urls = fetch_videos_urls_from_playlist(url)
-    download_videos(urls, dir)
+def fetch(dir, urls):
+
+    # if urls provided is a path to a file then we simply
+    # parse this file for urls and get all the urls into one list:
+    if path.exists(urls):
+        urls_list = parse_urls_from_file(urls)
+    # playlist scraping:
+    elif "playlist" in urls:
+        urls_list = fetch_videos_urls_from_playlist(urls)
+    # direct video url
+    else:
+        urls_list = [urls]
+
+    download_videos(urls_list, dir)
+
+def parse_urls_from_file(path):
+    urls = []
+    with open(path, "r") as _file:
+        for _line in _file:
+            if "playlist" in _line:
+                _urls = fetch_videos_urls_from_playlist(_line)
+                urls += _urls
+            else:
+                urls.append(_line)
+    return urls
 
 def fetch_videos_urls_from_playlist(playlist_url):
     res = requests.get(playlist_url)
@@ -43,7 +66,14 @@ def download_videos(urls, dir):
                _video.title
                ))
         best = _video.getbest()
-        best.download(quiet=False, filepath=dir)
+        _filepath = "%s/%s.%s" % (
+            dir,
+            _video.title.replace(':',''),
+            best.extension)
+        if not path.exists(_filepath):
+            best.download(quiet=False, filepath=_filepath)
+        else:
+            print("File exist - skipping: %s\n" % _filepath)
 
 if __name__ == '__main__':
     fetch()
